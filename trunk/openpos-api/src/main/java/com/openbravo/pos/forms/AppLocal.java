195ea6f21a72ed29d2102d44ff4f9b1e998cf4b5
niteg8@googlemail.com <niteg8@googlemail.com@4860adc0-00c8-ef64-0a7a-6ec5318e897f>
2011-07-01 11:18:34 +0000

    added extra constant to distinguish app and db version

--- trunk/openpos-api/src/main/java/com/openbravo/pos/forms/AppLocal.java
+++ trunk/openpos-api/src/main/java/com/openbravo/pos/forms/AppLocal.java
@@ -22,33 +22,34 @@ package com.openbravo.pos.forms;
 import com.openbravo.beans.LocaleResources;
 
 /**
- *
+ * 
  * @author adrianromero
  */
 public class AppLocal {
-    
-    public static final String APP_NAME = "Openbravo POS";
-    public static final String APP_ID = "openbravopos";
-    public static final String APP_VERSION = "2.30.2";
-  
-    // private static List<ResourceBundle> m_messages;
-    private static LocaleResources m_resources;
-    
-    static {
-        m_resources = new LocaleResources();
-        m_resources.addBundleName("pos_messages");
-        m_resources.addBundleName("erp_messages");
-    }
-    
-    /** Creates a new instance of AppLocal */
-    private AppLocal() {
-    }
-    
-    public static String getIntString(String sKey) {
-        return m_resources.getString(sKey);
-    }
-    
-    public static String getIntString(String sKey, Object ... sValues) {
-        return m_resources.getString(sKey, sValues);
-    }
+
+	public static final String APP_NAME = "Openbravo POS";
+	public static final String APP_ID = "openbravopos";
+	public static final String APP_VERSION = "2.30.2";
+	public static final String DB_VERSION = "2.40.0";
+
+	// private static List<ResourceBundle> m_messages;
+	private static LocaleResources m_resources;
+
+	static {
+		m_resources = new LocaleResources();
+		m_resources.addBundleName("pos_messages");
+		m_resources.addBundleName("erp_messages");
+	}
+
+	/** Creates a new instance of AppLocal */
+	private AppLocal() {
+	}
+
+	public static String getIntString(String sKey) {
+		return m_resources.getString(sKey);
+	}
+
+	public static String getIntString(String sKey, Object... sValues) {
+		return m_resources.getString(sKey, sValues);
+	}
 }

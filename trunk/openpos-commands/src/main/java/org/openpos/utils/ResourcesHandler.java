ec6c46307bacd64aac20587e3b1e08216d080a2e
niteg8@googlemail.com <niteg8@googlemail.com@4860adc0-00c8-ef64-0a7a-6ec5318e897f>
2013-11-18 10:33:42 +0000

    added wifi storing

--- trunk/openpos-commands/src/main/java/org/openpos/utils/ResourcesHandler.java
+++ trunk/openpos-commands/src/main/java/org/openpos/utils/ResourcesHandler.java
@@ -1,5 +1,7 @@
 package org.openpos.utils;
 
+import java.io.UnsupportedEncodingException;
+
 import com.openbravo.pos.forms.AppView;
 import com.openbravo.pos.forms.DataLogicSystem;
 
@@ -9,5 +11,18 @@ public abstract class ResourcesHandler {
 
 	protected ResourcesHandler(AppView app) {
 		dataLogicSystem = (DataLogicSystem)app.getBean("com.openbravo.pos.forms.DataLogicSystem");
+	}
+
+	protected String getResourceAsString(String key) {
+		return dataLogicSystem.getResourceAsText(key);
+	}
+
+	protected void setResourceAsString(String key, String value) {
+		try {
+			dataLogicSystem.setResource(key, 0, value.getBytes("UTF8"));
+		}
+		catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
